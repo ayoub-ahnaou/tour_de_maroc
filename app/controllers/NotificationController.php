0fb680c8f6b7b8d3ec237656f77eb1b0b12757a0
taouamwalideddine <wtaouam@gmail.com>
2025-02-11 19:41:48 +0100

    first commit

---
+++ app/controllers/NotificationController.php
@@ -0,0 +1,30 @@
+<?php
+
+use PDO;
+
+class NotificationModel {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    // Fetch notifications for a specific user
+    public function getNotificationsByUser($receiver_id) {
+        $query = "SELECT * FROM Notification WHERE destinataire_id = :receiver_id ORDER BY notification_id DESC";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute(['receiver_id' => $receiver_id]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Add a new notification
+    public function addNotification($content, $receiver_id, $etape_id = null) {
+        $query = "INSERT INTO Notification (contenu, destinataire_id, etape_id) VALUES (:content, :receiver_id, :etape_id)";
+        $stmt = $this->db->prepare($query);
+        return $stmt->execute([
+            'content' => $content,
+            'receiver_id' => $receiver_id,
+            'etape_id' => $etape_id
+        ]);
+    }
+}
