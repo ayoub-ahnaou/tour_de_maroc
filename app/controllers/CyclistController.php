6b0352e22b75f9654386836eff44265db3da5e7c
MrNobodyCode <essadeq.billouche@gmail.com>
2025-02-12 15:08:45 +0100

    Cyclist model view controller

--- app/controllers/CyclistController.php
+++ app/controllers/CyclistController.php
@@ -1,8 +1,21 @@
 <?php
 
-namespace TourDeMaroc\App\controllers;
 
-class CyclistController
+class CyclistController extends Controller
 {
+    private $CylistModel;
+
+    public function __construct()
+    {
+
+        $this->CylistModel = $this->modal('CyclistModel');
+    }
+
+    public function CyclistProfile($id)
+    {
+        $CylistDetails = $this->CylistModel->getCyclistById($id);
+        $data = ["name" => "Test", 'Age' => '23'];
+        $this->view('CyclistProfile', $data);
+    }
 
 }
--- app/models/CyclistModel.php
+++ app/models/CyclistModel.php
@@ -1,8 +1,28 @@
 <?php
 
-namespace TourDeMaroc\App\models;
+use TourDeMaroc\App\Entity\Cycliste;
+
+require_once '../app/libraries/Database.php';
+require_once '../app/Entity/Cycliste.php';
+
 
 class CyclistModel
 {
+    private $db;
 
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+
+    public function getCyclistById($id)
+    {
+        $stmt = $this->db->prepare('SELECT * from cycliste where cycliste_id = :id');
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return new Cycliste()
+        var_dump($result);
+    }
 }
