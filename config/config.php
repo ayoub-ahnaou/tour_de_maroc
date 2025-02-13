1c1f2f8056f27c2b123a705d51b7781d10e6a4fb
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-02-13 15:30:42 +0100

    Remove config.php from version control and update .gitignore to exclude it

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("HOST_NAME", "localhost");
-define("DATABASE_NAME", "tour_de_maroc_db");
-define("USER_NAME", "postgres");
-define("PASSWORD", "");
-
-define("URL_ROOT", "http://localhost/tour_de_maroc");
-define('APP_ROOT', dirname(dirname(__FILE__)));
