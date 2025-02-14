d456508afff10cfaa041e8f3f7eb83128d916224
taouamwalideddine <wtaouam@gmail.com>
2025-02-14 10:35:08 +0100

    'help me' changed in the database

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define("HOST_NAME", "localhost");
-define("DATABASE_NAME", "tour_de_maroc_db");
+define("DATABASE_NAME", "tour_du_maroc_db");
 define("USER_NAME", "postgres");
 define("PASSWORD", "1111");
 
